63a889fde0ae92ec6eff2e40f7ae5071f0c3dd9d
Applefa4ry <apple.f4iry@yandex.ru>
2023-02-28 21:21:10 +0300

    всё исправила

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,25 @@
 let editButton = document.querySelector(".profile__edit-button");
 let closeButton = document.querySelector(".popup__close");
-
-editButton.addEventListener("click", openPopUp);
-editButton.addEventListener("click", inputFieldFill);
-closeButton.addEventListener("click", openPopUp);
-
-function openPopUp(){
-  document.querySelector(".popup").classList.toggle("popup_opened");
-}
-
-// Находим форму в DOM
 let formElement = document.querySelector(".popup__container");
 let nameInput = document.querySelectorAll(".popup__field")[0];
 let jobInput = document.querySelectorAll(".popup__field")[1];
-
 let nameProfile = document.querySelector(".profile__name");
 let aboutProfile = document.querySelector(".profile__about");
+
+
+function openPopUp(){
+  document.querySelector(".popup").classList.add("popup_opened");
+  inputFieldFill();
+}
+
+function closePopUp(){
+  document.querySelector(".popup").classList.remove("popup_opened");
+}
 
 function inputFieldFill(){
   nameInput.value = nameProfile.innerHTML;
   jobInput.value = aboutProfile.innerHTML;
 }
-
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
@@ -33,3 +31,5 @@ function handleFormSubmit (evt) {
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
+editButton.addEventListener("click", openPopUp);
+closeButton.addEventListener("click", closePopUp);
