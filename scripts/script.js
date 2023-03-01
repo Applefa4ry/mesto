c3f784ae1bf2682754fc84d773a553479f2c0970
Applefa4ry <apple.f4iry@yandex.ru>
2023-03-01 23:14:29 +0300

    исправила

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,19 @@
 let editButton = document.querySelector(".profile__edit-button");
 let closeButton = document.querySelector(".popup__close");
 let formElement = document.querySelector(".popup__container");
-let nameInput = document.querySelectorAll(".popup__field")[0];
-let jobInput = document.querySelectorAll(".popup__field")[1];
+let nameInput = document.querySelectorAll(".form__field")[0];
+let jobInput = document.querySelectorAll(".form__field")[1];
 let nameProfile = document.querySelector(".profile__name");
 let aboutProfile = document.querySelector(".profile__about");
-
+let popup = document.querySelector(".popup");
 
 function openPopUp(){
-  document.querySelector(".popup").classList.add("popup_opened");
+  popup.classList.add("popup_opened");
   inputFieldFill();
 }
 
 function closePopUp(){
-  document.querySelector(".popup").classList.remove("popup_opened");
+  popup.classList.remove("popup_opened");
 }
 
 function inputFieldFill(){
@@ -27,7 +27,7 @@ function handleFormSubmit (evt) {
     let jobValue = jobInput.value;
     nameProfile.textContent = nameValue;
     aboutProfile.textContent = jobValue;
-    document.querySelector(".popup").classList.toggle("popup_opened");
+    popup.classList.toggle("popup_opened");
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
