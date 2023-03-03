cc28687ed1b84fbc29066e82b5e170c357d0aa91
Applefa4ry <apple.f4iry@yandex.ru>
2023-03-03 23:35:20 +0300

    исправила по БЭМ

--- scripts/validate.js
+++ scripts/validate.js
@@ -60,7 +60,7 @@ enableValidation({
   formSelector: '.form',
   inputSelector: '.form__field',
   submitButtonSelector: '.form__button-submit',
-  inactiveButtonClass: 'button-submit_disabled',
+  inactiveButtonClass: 'form__button-submit_disabled',
   inputErrorClass: 'popup__input_type_error',
   errorClass: 'popup__error_visible'
 });
