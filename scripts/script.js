2982792fa474d198605b0247c81be737eae46b1a
Applefa4ry <apple.f4iry@yandex.ru>
2023-03-03 15:28:04 +0300

    исправила нажатие на лайк и открытие попапа

--- scripts/script.js
+++ scripts/script.js
@@ -86,7 +86,7 @@ function addCard(imgSrcValue, titleValue) {
   })
 
   cardElement.addEventListener("click", function(evt){
-    if(evt.target !== cardElement.querySelector(".card__trash")){
+    if(evt.target !== cardElement.querySelector(".card__trash") && evt.target !== cardElement.querySelector(".card__like")){
       openPopUp(popups[2]);
       fillPopupPicture(cardElement);
     }
@@ -119,7 +119,7 @@ card.forEach(function(element){
   });
   element.querySelector(".card__trash").addEventListener("click", removeCard);
   element.addEventListener("click", function(evt){
-    if(evt.target !== element.querySelector(".card__trash")){
+    if(evt.target !== element.querySelector(".card__trash") && evt.target !== cardElement.querySelector(".card__like")){
       openPopUp(popups[2]);
       fillPopupPicture(element);
     }
